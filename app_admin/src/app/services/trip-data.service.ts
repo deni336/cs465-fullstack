145312360c09fe88ee18fda9d0b601150a2d7e23
Carson Denison <denison_carson@hotmail.com>
2025-04-14 01:08:09 -0500

    Module 6 completed baseline

---
+++ app_admin/src/app/services/trip-data.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { Trip } from '../models/trips';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class TripDataService {
+
+  constructor(private http: HttpClient) { }
+    url = 'http://localhost:3000/api/trips';
+  getTrips(): Observable<Trip[]> {
+    return this.http.get<Trip[]>(this.url);
+  }
+
+  addTrip(formData: Trip) : Observable<Trip> {
+    return this.http.post<Trip>(this.url, formData);
+  }
+
+  getTrip(tripCode: string) : Observable<Trip> {
+    return this.http.get<Trip>(this.url + '/' + tripCode);
+  }
+
+  deleteTrip(id: string) : Observable<Trip> {
+    return this.http.delete<Trip>(this.url + '/' + id);
+  }
+
+  updateTrip(formData: Trip) : Observable<Trip> {
+    return this.http.put<Trip>(this.url + '/' + formData._id, formData);
+  }
+}
--- travlr/app_api/controllers/trips.js
+++ travlr/app_api/controllers/trips.js
@@ -40,8 +40,64 @@ const tripsList = async(req, res) => {
    }
 };
 
+const tripsAddTrip = async (req, res) => {
+   try {
+     const trip = await Model.create({
+       code: req.body.code,
+       name: req.body.name,
+       length: req.body.length,
+       start: req.body.start,
+       resort: req.body.resort,
+       perPerson: req.body.perPerson,
+       image: req.body.image,
+       description: req.body.description
+     });
+     return res.status(201).json(trip);
+   } catch (err) {
+     return res.status(400).json(err);
+   }
+ };
+
+ // PUT: /trips/:tripCode - Adds a new Trip  
+ // // Regardless of outcome, response must include HTML status code  
+ // // and JSON message to the requesting client  
+ const tripsUpdateTrip = async(req, res) => {
+   // Uncomment for debugging       
+   console.log(req.params);
+   console.log(req.body);
+   const q = await Model
+   .findOneAndUpdate(
+      {'code': req.params.tripCode },
+      {
+         code: req.body.code,
+         name: req.body.name,
+         length: req.body.length,
+         start: req.body.start,
+         resort: req.body.resort,
+         perPerson: req.body.perPerson,
+         image: req.body.image,
+         description: req.body.description
+      }
+   )          
+   .exec();
+   if(!q)
+   { // Database returned no data
+      return res
+      .status(400)
+      .json(err);
+   } else { // Return resulting updated trip
+      return res
+      .status(201)
+      .json(q);
+   } // Uncomment the following line to show results of operation          
+   // // on the console          
+   // // console.log(q);  
+   };  
+
 module.exports = {
    tripsList,
-   tripsFindByCode
+   tripsFindByCode,
+   tripsAddTrip,
+   tripsUpdateTrip
 };
 
